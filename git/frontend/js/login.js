2b20bc0b1cb41bfa9acd4fe1dd2f44c13f9bafa7
CitlalySORomero22 <1718110401@utectulancingo.edu.mx>
2022-07-22 19:35:34 +0000

    listoo

--- git/frontend/js/login.js
+++ git/frontend/js/login.js
@@ -1,5 +1,4 @@
-function PostRegUser(){
-    var request = new XMLHttpRequest();
+function login(){   
 
     let email = document.getElementById("email");
     let password  = document.getElementById("password");  
@@ -11,21 +10,41 @@ function PostRegUser(){
     console.log(password.value );
     console.log(payload);
 
-    request.open('POST', "https://8000-citlalysoromero-fireapi-vgmru85w7q3.ws-us54.gitpod.io/users/",true);
+    var request = new XMLHttpRequest();
+    request.open('POST', "https://8000-citlalysoromero-fireapi-vgmru85w7q3.ws-us54.gitpod.io/users/token",true);
     request.setRequestHeader("accept", "application/json");
+    request.setRequestHeader("Authorization", "Basic " + btoa(payload.email + ":" + payload.password));
     request.setRequestHeader("Content-Type", "application/json");
  
     request.onload = () => {
+
+        var response = request.response;
+        console.log(response);
         if (request.status === 401 || request.status === 403) {
             alert(json.detail);
+            Swal.fire({
+                title: json.detail,
+                text: json.detail,
+                type: "error"
+            }).then(function() {
+                window.location = "/templates/login.html";
+            });
         }
-        else if (request.status == 202){
+        else if (request.status == 202) {
             const response = request.responseText;
             const json = JSON.parse(response);
             console-console.log(json);
-            alert("Usuario agregado");
+            sessionStorage.setItem("token", json.token);
 
-        }  
+            Swal.fire({
+                title: "Ya estas registrado(a)",
+                text: "Bienvenido(a)",
+                type: "success"
+            }).then(function() {
+                window.location = "../templates/bienvenida.html";
+            });
+        };
+    
     };
     request.send(JSON.stringify(payload));
 }
